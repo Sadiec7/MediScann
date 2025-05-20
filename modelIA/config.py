72e8863a12ec023c16a6b5cf80e0070fcded8646
Sadiec7 <saldiec7@gmail.com>
2025-05-20 10:33:18 -0600

    Implementation dental model

--- modelIA/config.py
+++ modelIA/config.py
@@ -5,3 +5,4 @@ class Config:
     PORT = int(os.getenv("PORT", 5000))
     MODEL_PATH = os.getenv("MODEL_FILE", "skindisease_full.h5")
     CLASSES_PATH = os.getenv("CLASS_NAMES_FILE", "class_names.txt")
+    SECOND_MODEL_FILE = os.getenv("SECOND_MODEL_FILE", "dentalalpha.h5")
